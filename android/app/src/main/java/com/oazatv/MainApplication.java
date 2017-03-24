d4c4dd74a77d78463a6de5f5b462e93e2fef45ed
Michal Mlejnek <chaemil72@gmail.com>
2017-03-24 15:39:47 +0100

    Fix iOS build

--- android/app/src/main/java/com/oazatv/MainApplication.java
+++ android/app/src/main/java/com/oazatv/MainApplication.java
@@ -4,6 +4,12 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
+import com.brentvatne.react.ReactVideoPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
+import cl.json.RNSharePackage;
+import com.github.xinthink.rnmk.ReactMaterialKitPackage;
+import com.babisoft.ReactNativeLocalization.ReactNativeLocalizationPackage;
+import com.i18n.reactnativei18n.ReactNativeI18n;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,7 +30,13 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new ReactVideoPackage(),
+            new VectorIconsPackage(),
+            new RNSharePackage(),
+            new ReactMaterialKitPackage(),
+            new ReactNativeLocalizationPackage(),
+            new ReactNativeI18n()
       );
     }
   };
